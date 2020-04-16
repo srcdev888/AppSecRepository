fa293d0107311feca1be5a620b6d5d119f1068d2
cxdemosg <cxdemosg@gmail.com>
2020-04-16 14:20:28 +0800

    Space

--- docker/psmailer/SendEmail.ps1
+++ docker/psmailer/SendEmail.ps1
@@ -1,40 +1,57 @@
 # Powershell script to send email via 'Send-MailMessage'
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/send-mailmessage
+
 <#
 .SYNOPSIS
 Powershell script to send email via 'Send-MailMessage'
+
 .DESCRIPTION
-.
+Powershell script to send email via 'Send-MailMessage'
+
 .PARAMETER from
 Email author
+
 .PARAMETER to
 Receipients, support multiple separated by ','
+
 .PARAMETER subject
 Subject
+
 .PARAMETER body
 Body
+
 .PARAMETER bodyAsHtml
 Body includes HTML
+
 .PARAMETER attachments
 Attachments, support multiple separated by ','
+
 .PARAMETER smtpServer
 SMTP Server URL
+
 .PARAMETER smtpPort
 SMTP Server port
+
 .PARAMETER smtpUsername
 SMTP login username
+
 .PARAMETER smtpPassword
 SMTP login password
+
 .PARAMETER smtpUseSSL
 SMTP use SSL, default=true
+
 .PARAMETER DeliveryNotificationOption
 Delivery notification option
+
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/send-mailmessage
+
 .NOTES
 Author: Pedric Kng
 Date:   April 15, 2020
 #>
+
 param(
     [Parameter(Mandatory, HelpMessage="From")][string]$from,
     [Parameter(Mandatory, HelpMessage="To")][string[]]$to,
