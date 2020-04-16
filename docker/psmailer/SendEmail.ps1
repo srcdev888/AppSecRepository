92aea83f89adf5c377164dba5a9fe97a91aba867
cxdemosg <cxdemosg@gmail.com>
2020-04-16 14:41:27 +0800

    Moved help message to bottom of file

--- docker/psmailer/SendEmail.ps1
+++ docker/psmailer/SendEmail.ps1
@@ -1,4 +1,41 @@
-<#
+# Powershell script to send email
+
+param(
+    [Parameter(Mandatory, HelpMessage="From")][string]$from,
+    [Parameter(Mandatory, HelpMessage="To")][string[]]$to,
+    [Parameter(Mandatory, HelpMessage="Subject")][string]$subject,
+    [Parameter(Mandatory, HelpMessage="Body")][string]$body,
+    [Parameter(HelpMessage="Body includes HTML")][switch]$bodyAsHtml = $true,
+    [Parameter(HelpMessage="Attachments")][string[]]$attachments,
+    [Parameter(Mandatory, HelpMessage="Smtp Server")][string]$smtpServer,
+    [Parameter(HelpMessage="Smtp Port, default=587")][string]$smtpPort=587,
+    [Parameter(Mandatory, HelpMessage="Smtp Username")][string]$smtpUsername,
+    [Parameter(Mandatory, HelpMessage="Smtp Password")][string]$smtpPassword,
+    [Parameter(HelpMessage="Smtp Use Ssl, default=true")][switch]$smtpUseSSL = $true,
+    [Parameter(HelpMessage="DeliveryNotificationOption")][string[]]$DeliveryNotificationOption
+)
+
+$smtpSecurePassword = ConvertTo-SecureString $smtpPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($smtpUsername, $smtpSecurePassword)
+
+$sendMailParams = @{
+    From = $from
+    To = $to
+    Subject = $subject
+    Body = $body
+    BodyAsHtml = $true
+    SMTPServer = $smtpServer
+    Port = $smtpPort
+    UseSsl = $smtpUseSSL
+    Credential = $credential
+}
+
+if ($attachments.count -gt 0 ) { $sendMailParams.Add('Attachments',$attachments)}
+if ($DeliveryNotificationOption.count -gt 0 ) { $sendMailParams.Add('DeliveryNotificationOption', $DeliveryNotificationOption) }
+
+Send-MailMessage @sendMailParams
+
+<# 
 .SYNOPSIS
 Powershell script to send email via 'Send-MailMessage'
 
@@ -47,39 +84,5 @@ https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/
 .NOTES
 Author: Pedric Kng
 Date:   April 15, 2020
+
 #>
-
-param(
-    [Parameter(Mandatory, HelpMessage="From")][string]$from,
-    [Parameter(Mandatory, HelpMessage="To")][string[]]$to,
-    [Parameter(Mandatory, HelpMessage="Subject")][string]$subject,
-    [Parameter(Mandatory, HelpMessage="Body")][string]$body,
-    [Parameter(HelpMessage="Body includes HTML")][switch]$bodyAsHtml = $true,
-    [Parameter(HelpMessage="Attachments")][string[]]$attachments,
-    [Parameter(Mandatory, HelpMessage="Smtp Server")][string]$smtpServer,
-    [Parameter(HelpMessage="Smtp Port, default=587")][string]$smtpPort=587,
-    [Parameter(Mandatory, HelpMessage="Smtp Username")][string]$smtpUsername,
-    [Parameter(Mandatory, HelpMessage="Smtp Password")][string]$smtpPassword,
-    [Parameter(HelpMessage="Smtp Use Ssl, default=true")][switch]$smtpUseSSL = $true,
-    [Parameter(HelpMessage="DeliveryNotificationOption")][string[]]$DeliveryNotificationOption
-)
-
-$smtpSecurePassword = ConvertTo-SecureString $smtpPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($smtpUsername, $smtpSecurePassword)
-
-$sendMailParams = @{
-    From = $from
-    To = $to
-    Subject = $subject
-    Body = $body
-    BodyAsHtml = $true
-    SMTPServer = $smtpServer
-    Port = $smtpPort
-    UseSsl = $smtpUseSSL
-    Credential = $credential
-}
-
-if ($attachments.count -gt 0 ) { $sendMailParams.Add('Attachments',$attachments)}
-if ($DeliveryNotificationOption.count -gt 0 ) { $sendMailParams.Add('DeliveryNotificationOption', $DeliveryNotificationOption) }
-
-Send-MailMessage @sendMailParams
